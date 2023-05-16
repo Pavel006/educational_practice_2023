7d4e60c7410a8863db333d99f6bb2ff29a538c67
pavel <pavel473945814@gmail.com>
2023-05-16 20:15:14 +0300

    f

--- Кречетов/Program.cs
+++ Кречетов/Program.cs
@@ -12,22 +12,48 @@ namespace Кречетов
     {
         static void Main(string[] args)
         {
-            string archivePath = @"C:\Users\pavel\source\repos\Кречетов\Кречетов\bin\Debug\repos.zip";
-            FileStream fileStream1 = File.Open(archivePath, FileMode.Open);
-            ZipArchive archive = new ZipArchive(fileStream1, ZipArchiveMode.Update);
-            foreach (ZipArchiveEntry entry in archive.Entries)
+            Console.Write("Введите номер файла и путь к архиву через пробел: \n> ");
+            string InputString = Convert.ToString(Console.ReadLine());
+            Unarchiver.Unarchiv(Convert.ToInt32(InputString.Split(' ')[0]), InputString.Split(' ')[1]);
+            //string archivePath = @"C:\Users\pavel\source\repos\Кречетов\Кречетов\bin\Debug\repos.zip";
+            //FileStream fileStream1 = File.Open(archivePath, FileMode.Open);
+            //ZipArchive archive = new ZipArchive(fileStream1, ZipArchiveMode.Update);
+            //foreach (ZipArchiveEntry entry in archive.Entries)
+            //{
+            //    if (entry.Name != "")
+            //    {
+            //        Console.WriteLine($"Имя файла: {entry.Name} " +
+            //            $"Размер сжатого файла: {entry.CompressedLength}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Папка: {entry.FullName}");
+            //    }
+            //}
+            //Console.Read();
+        }
+
+        public class Unarchiver
+        {
+            public static void Unarchiv(int number, string archivePath)
             {
-                if (entry.Name != "")
+                FileStream fileStream = File.Open(archivePath, FileMode.Open);
+                ZipArchive archive = new ZipArchive(fileStream, ZipArchiveMode.Update);
+                var location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                var programmPath = Path.GetDirectoryName(location);
+                byte[] buffer = Encoding.Default.GetBytes("");
+                if (archive.Entries.Count() < number)
                 {
-                    Console.WriteLine($"Имя файла: {entry.Name} " +
-                        $"Размер сжатого файла: {entry.CompressedLength}");
+                    using (FileStream fstream = File.Open(programmPath + $"{number}_{fileStream.Name}", FileMode.Create))
+                    {
+
+                    }
                 }
-                else
+                foreach (ZipArchiveEntry entry in archive.Entries)
                 {
-                    Console.WriteLine($"Папка: {entry.FullName}");
+
                 }
             }
-            Console.Read();
         }
     }
 }
